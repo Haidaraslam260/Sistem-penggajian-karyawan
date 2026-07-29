88302fc6c070e7cfcebaf2aaefaee0962a11f425
Haidaraslam260 <aslamhaidar98@gmail.com>
2026-07-29 11:44:23 +0700

    refactor: map Position id column to positions_id in PostgreSQL database

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 // Koordinat lokasi kantor pusat (Contoh: Bundaran HI, Jakarta)
-export const OFFICE_LATITUDE = -6.356603;
-export const OFFICE_LONGITUDE = 106.833422;
+export const OFFICE_LATITUDE = -6.3930222896875275;
+export const OFFICE_LONGITUDE = 106.8211866062272;
 
 // Batas radius absensi yang diizinkan (dalam satuan meter)
 export const MAX_RADIUS_METERS = 100;
